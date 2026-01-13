dee230ddcd8dc2c26e8782bafaed537f0d160992
Pratyush Kattel <pratyushkatel@gmail.com>
2026-01-13 22:51:51 +0545

    database __init__

---
+++ api_serve/database.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine,Column,Integer,String,DateTime,Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+
+DATABASE_URL="sqlite:///.movie_reccomender.db"
+
+engine=create_engine(
+    DATABASE_URL,
+    connect_args={'check_same_thread':False} if 'sqlite' in DATABASE_URL else {}
+)
+
+sessionlocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
+Base=declarative_base()
+
+class SearchHistory(Base):
+
+    """Table that tracks every single search made by user"""
+    __tablename__="search_history"
+    id=Column(Integer,primary_key=True,index=True)
+    movie_searched=Column(String)
+    reccomendations=Column(Text)
+    movie_name=Column(String)
+    timestamp=Column(DateTime,default=datetime.utcnow)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    print("Alll the tables have been created")
+
+def get_db():
+    db=sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- api_serve/main.py
+++ api_serve/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Request,Form
+from fastapi import FastAPI, Request,Form,Depends
 from fastapi.responses import FileResponse
 from fastapi.exceptions import HTTPException
 import pandas as pd
 import numpy as np
+from database import init_db,get_db,SearchHistory
+from sqlalchemy.orm import Session
+import json
 from contextlib import asynccontextmanager
 # from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -12,6 +15,7 @@ async def lifespan(app: FastAPI):
     # my startup_code , run once
     print("Loading the similarity matrix ......")
     matrix=np.load("project/similarity_matrix.pkl",allow_pickle=True)
+    init_db()
     yield {'similarity_matrix':matrix}
 
     print("shutting down....")
@@ -28,18 +32,25 @@ async def read_index():
     return FileResponse('static/index.html')
    
 @app.post("/submit")
-async def handle_form(request:Request,movie_name:str=Form(...)):
+async def handle_form(request:Request,movie_name:str=Form(...),db:Session=Depends(get_db)):
     df=pd.read_parquet("api_serve/movies_list.parquet")
     similarity=request.state.similarity_matrix   
     try:
         index=df[df['movie_name'] == movie_name].index[0]
 
     except IndexError:
-        raise HTTPException(status_code=404, detail="Movie not found")    
+        raise HTTPException(status_code=404, detail="Movie not found")  
+      
     distances = similarity[index]
     movie_indices = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     recommended_movies = [df.iloc[i[0]]['movie_name'] for i in movie_indices]  # Fixed column name
     # print(recommended_movies)
+    search_record=SearchHistory(
+        movie_searched=movie_name,
+        reccomendations=json.dumps(recommended_movies)
+    )
+    db.add(search_record)
+    db.commit()
     return {'message':f"server receives the message {movie_name}",
             'recommended_movies':recommended_movies}
 
